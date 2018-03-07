2844555c3ff438adf29e54bbbeece751c7f2ba9d
Andreas Hübner <a.huebner@itsonix.eu>
2018-03-07 08:22:17 +0100

    Added Installation of Plugins

--- vim/install.sh
+++ vim/install.sh
@@ -6,3 +6,5 @@ cp init.vim ~/.config/nvim/init.vim
 if [ ! -d ~/.config/nvim/bundle ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
 fi
+
+nvim +PluginInstall +qall
