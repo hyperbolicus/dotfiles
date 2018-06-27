ef21da8039994e9d1f5374c0e393f4c7e61847d7
Andreas Hübner <a.huebner@finatix.de>
2018-06-27 08:34:30 +0200

    .UPDATE added misc packages for macos

---
+++ misc/macos.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+brew cask install firefox
+brew cask install oversight
+brew cask install keepassxc
+brew install tmux
+brew cask install docker
+brew cask install vagrant
+brew cask install packer
+brew install htop
+brew install python3
+brew install nvim
+brew install ctags
