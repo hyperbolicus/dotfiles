e9f636c219b9d577e0ddf832d6b5fdbb09897bc1
hyperbolicus <hyperbolicus@arcor.de>
2022-01-16 22:28:51 +0100

    feature: added basic vim plugins

--- nix/nvim/default.nix
+++ nix/nvim/default.nix
@@ -1,24 +1,49 @@
-self: super: {
-	neovim =  super.neovim.override {
-          viAlias = true;
-          vimAlias = true;
-          configure = {
-            customRC = ''
-            set hidden
-            set noswapfile
-            set smartcase
-            set smarttab
-            set smartindent
-            set autoindent
-            set expandtab
-            set shiftwidth=2
-            set softtabstop=2
-            set background=dark
-            set laststatus=2
-            set ignorecase
-            set wildmode=longest,list,full
-            set nu
-            '';
+  self: super: {
+    vim-lsp-settings = super.vimUtils.buildVimPluginFrom2Nix {
+      name = "vim-lsp-settings";
+      buildDependencies = [super.shellcheck];
+      src = super.fetchFromGitHub {
+        owner = "mattn";
+        repo = "vim-lsp-settings";
+        rev = "c796dd1878bede91c110a858123368861fc5aa34";
+        sha256 = "XK1Y2qVe3ajjGmRdy/fJ6Nnb7PyHYCntJFJyjqr4uB4=";
+      };
+    };
+
+    neovim =  super.neovim.override {
+      viAlias = true;
+      vimAlias = true;
+      configure = {
+        customRC = ''
+              syntax enable
+              set nocompatible
+
+              filetype plugin indent on
+
+
+              set hidden
+              set noswapfile
+              set smartcase
+              set smarttab
+              set smartindent
+              set autoindent
+              set expandtab
+              set shiftwidth=2
+              set softtabstop=2
+              set background=dark
+              set laststatus=2
+              set ignorecase
+              set wildmode=longest,list,full
+              set nu
+        '';
+        packages.myVimPackages = {
+          start = [
+            super.vimPlugins.vim-lsp
+            super.vimPlugins.vim-nix
+            self.vim-lsp-settings
+          ];
+
         };
-  };
+      };
+    };
   }
