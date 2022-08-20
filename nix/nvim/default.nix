0eff40d0cfd66882717e4c68839a3c647f8e4367
hyperbolicus <hyperbolicus@arcor.de>
2022-08-20 10:29:46 +0200

    fix: fixed vim plugins

--- nix/nvim/default.nix
+++ nix/nvim/default.nix
@@ -1,3 +1,27 @@
+let 
+   vim-lsp-settings = super: (super.vimUtils.buildVimPluginFrom2Nix {
+        name = "vim-lsp-settings";
+        buildDependencies = [super.shellcheck];
+        src = super.fetchFromGitHub {
+          owner = "mattn";
+          repo = "vim-lsp-settings";
+          rev = "c796dd1878bede91c110a858123368861fc5aa34";
+          sha256 = "XK1Y2qVe3ajjGmRdy/fJ6Nnb7PyHYCntJFJyjqr4uB4=";
+        };
+      });
+      asyncomplete-omni = super: (super.vimUtils.buildVimPluginFrom2Nix {
+        name = "asyncomplete-omni";
+        buildDependencies = [super.shellcheck];
+        src = super.fetchFromGitHub {
+          owner = "yami-beta";
+          repo = "asyncomplete-omni.vim";
+          rev = "f13986b671a37d6320476af6bc066697e71463c1";
+          sha256 = "RtHr7J+B95/SWzGwPp9vaktUWj1rsw3xioBbgRK+HPg=";
+        };
+      });
+
+in
+
 self: super: {
 
   neovim = super.neovim.override {
@@ -24,38 +48,17 @@ self: super: {
               set wildmode=longest,list,full
               set nu
       '';
-      vam.knownPlugins = super.vimPlugins // ({
-        vim-lsp-settings = super.vimUtils.buildVimPluginFrom2Nix {
-          name = "vim-lsp-settings";
-          buildDependencies = [super.shellcheck];
-          src = super.fetchFromGitHub {
-            owner = "mattn";
-            repo = "vim-lsp-settings";
-            rev = "c796dd1878bede91c110a858123368861fc5aa34";
-            sha256 = "XK1Y2qVe3ajjGmRdy/fJ6Nnb7PyHYCntJFJyjqr4uB4=";
-          };
-        };
-        asyncomplete-omni = super.vimUtils.buildVimPluginFrom2Nix {
-          name = "asyncomplete-omni";
-          buildDependencies = [super.shellcheck];
-          src = super.fetchFromGitHub {
-            owner = "yami-beta";
-            repo = "asyncomplete-omni.vim";
-            rev = "f13986b671a37d6320476af6bc066697e71463c1";
-            sha256 = "RtHr7J+B95/SWzGwPp9vaktUWj1rsw3xioBbgRK+HPg=";
-          };
-        };
-      }); 
-      vam.pluginDictionaries = [
-        "vim-nix"
-        "vim-lsp"
-        "vim-lsp-settings"
-        "dhall-vim"
-        "robotframework-vim"
-        "asyncomplete-omni"
-        "asyncomplete-vim"
-        "asyncomplete-lsp-vim"
+         packages.myPlugins =  {
+        start = with super.vimPlugins ; [    vim-nix
+        vim-lsp
+        (vim-lsp-settings super)
+        dhall-vim
+        robotframework-vim
+        (asyncomplete-omni super)
+        asyncomplete-vim
+        asyncomplete-lsp-vim
       ];
     };
-  };}
+  };
+};}
 
