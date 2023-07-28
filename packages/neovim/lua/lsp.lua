1a328b2e05471b843bc71ee9730876f0f2373108
hyperbolicus <hyperbolicus@arcor.de>
2023-07-28 14:50:10 +0200

    fix: changed python language server

--- packages/neovim/lua/lsp.lua
+++ packages/neovim/lua/lsp.lua
@@ -1,5 +1,5 @@
 local lspconfig = require('lspconfig')
-lspconfig.pyright.setup {}
+lspconfig.pylsp.setup {}
 lspconfig.tsserver.setup {}
 lspconfig.rust_analyzer.setup {
   -- Server-specific settings. See `:help lspconfig-setup`
